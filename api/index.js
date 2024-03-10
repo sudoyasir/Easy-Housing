2c86247f5366bc7ce5074378260a8a18dc1f55c9
Yasir Nawaz <y451rmahar@gmail.com>
2024-03-10 22:13:57 +0500

    :package: Connected DataBase

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
 
 const app = express();
 
-app.listen(3000, () => {  
-  console.log('Server is running on port 3000');
-}); 
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
