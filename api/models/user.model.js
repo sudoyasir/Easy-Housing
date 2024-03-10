150ceed13bf66a6ba7f283e747172993bf0f1ff5
Yasir Nawaz <y451rmahar@gmail.com>
2024-03-10 22:20:58 +0500

    :package: Created User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
