cc1847082362c4c01280e2df9bb64dede9f0cce7
Yasir Nawaz <y451rmahar@gmail.com>
2024-03-10 16:05:25 +0500

    :page_facing_up: Created all of the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,20 @@
-rfc\
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./assets/pages/Home";
+import SignIn from "./assets/pages/SignIn";
+import SignUp from "./assets/pages/SignUp";
+import About from "./assets/pages/About";
+import Profile from "./assets/pages/Profile";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
