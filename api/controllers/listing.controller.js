1ea934d11c174e7b83744a7d70b15d08d655e7b0
Yasir Nawaz <y451rmahar@gmail.com>
2024-03-18 16:17:11 +0500

    :package: created update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -26,3 +26,26 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler("Listing not found", 404));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler("You can update your own listings!", 401));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    return res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
